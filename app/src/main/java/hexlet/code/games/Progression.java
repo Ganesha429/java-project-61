b9b0cd86b31b20289c563703edc082a5809541e4
Ganesha429 <132095053+Ganesha429@users.noreply.github.com>
2024-10-22 21:48:27 +0300

    update.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -12,29 +12,45 @@ public class Progression {
         Scanner scanner = new Scanner(System.in);
 
         while (successfulAttempts < 3) {
-            int length = random.nextInt(6) + 5;
-            int gapIndex = random.nextInt(length);
-            int step = random.nextInt(10) + 1;
-            int start = random.nextInt(10) + 1;
+            String[] progression = generateProgression(random);
+            String correctAnswer = progression[progression.length - 1];
+            progression[progression.length - 1] = "..";
 
-            String[] progression = new String[length];
-            String correctAnswer = "";
-
-            for (int i = 0; i < length; i++, start += step) {
-                if (i == gapIndex) {
-                    correctAnswer = String.valueOf(start);
-                    progression[i] = "..";
-                } else {
-                    progression[i] = String.valueOf(start);
-                }
-            }
-
-            System.out.println("Question: " + String.join(" ", progression));
-            System.out.print("Your answer: ");
-            String userAnswer = scanner.next();
+            askQuestion(progression);
+            String userAnswer = getUserAnswer(scanner);
 
             successfulAttempts = Engine.checkingForCorrectness(userName, userAnswer, correctAnswer, successfulAttempts);
             Engine.checkingTheResultOfGame(userName, successfulAttempts);
         }
     }
+
+    private static String[] generateProgression(Random random) {
+        int length = random.nextInt(6) + 5;
+        int gapIndex = random.nextInt(length);
+        int step = random.nextInt(10) + 1;
+        int start = random.nextInt(10) + 1;
+
+        String[] progression = new String[length + 1];
+        for (int i = 0; i < length; i++, start += step) {
+            if (i == gapIndex) {
+                progression[length] = String.valueOf(start); // Store correct answer at the end
+                progression[i] = "..";
+            } else {
+                progression[i] = String.valueOf(start);
+            }
+        }
+        return progression;
+    }
+
+    private static void askQuestion(String[] progression) {
+        System.out.println("Question: " + String.join(" ", progression));
+    }
+
+    private static String getUserAnswer(Scanner scanner) {
+        System.out.print("Your answer: ");
+        return scanner.next();
+    }
 }
+
+
+
