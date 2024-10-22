12b310de966e3cf490d9dbd02b83e0a3aa3eee09
Ganesha429 <132095053+Ganesha429@users.noreply.github.com>
2024-10-22 21:35:43 +0300

    Update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -7,7 +7,6 @@ import hexlet.code.Engine;
 public class Progression {
     public static void getProgression(int numOfGame) {
         String newUserName = Engine.getName(numOfGame); // greeting and receiving the player's name
-
         int counterOfSuccessfulAttempts = 0;
         Random random = new Random();
         int minLength = 5;
@@ -16,28 +15,23 @@ public class Progression {
             int lengthOfArray = random.nextInt(6) + minLength; //will return random length from 5 to 10
             int randomGap = random.nextInt(lengthOfArray); //will return random number of gap from 0 to length of array
             int randomStepOfProgression = random.nextInt(10) + 1; // return the first random step for
-                                                                         // progression from 1 to 10
             int firstNumber = random.nextInt(10) + 1; //will return the first random number from 1 to 10
-
+            
             String[] arrayOfNums = new String[lengthOfArray]; //massive of random numbers
             String trueAnswer = "";
 
             // fill each element with a random number from 0 to 99
-            for (int counter = 0; counter < lengthOfArray; counter++) {
-                if (counter == randomGap) {
-                    arrayOfNums[counter] = String.valueOf(firstNumber);
-                    firstNumber += randomStepOfProgression;
-                    trueAnswer = arrayOfNums[counter];
-                    arrayOfNums[counter] = "..";
+            for (int i = 0; i < lengthOfArray; i++) {
+                if (i == randomGap) {
+                    trueAnswer = arrayOfNums[i];
+                    arrayOfNums[i] = "..";
                 } else {
-                    arrayOfNums[counter] = String.valueOf(firstNumber);
-                    firstNumber += randomStepOfProgression;
+                    arrayOfNums[i] = String.valueOf(firstNumber);
                 }
+                firstNumber += randomStepOfProgression;
             }
-
-            String str = String.join(" ", arrayOfNums);
-            System.out.println("Question: " + str);
-
+            
+            System.out.println("Question: " + String.join(" ", arrayOfNums));
             Scanner scanner = new Scanner(System.in);
             String answerOfUser = scanner.next();
             System.out.println("Your answer: " + answerOfUser);
@@ -51,3 +45,4 @@ public class Progression {
         }
     }
 }
+
