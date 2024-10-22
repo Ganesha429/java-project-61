028801dc544951aadaff476625d22934ea1f12f7
Ganesha429 <132095053+Ganesha429@users.noreply.github.com>
2024-10-22 23:43:29 +0300

    Update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -7,31 +7,27 @@ public class Engine {
     public static String getName(int numOfGames) {
         System.out.println("\nWelcome to the Brain Games!");
         System.out.print("May I have your name? ");
+    
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
-        // rules output
-        switch (numOfGames) {
-            case 1:
-                break;
-            case 2:
-                System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-                break;
-            case 3:
-                System.out.println("What is the result of the expression?");
-                break;
-            case 4:
-                System.out.println("Find the greatest common divisor of given numbers.");
-                break;
-            case 5:
-                System.out.println("What number is missing in the progression?");
-                break;
-            case 6:
-                System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + numOfGames);
+    
+        String[] rules = {
+            null,
+            null,
+            "Answer 'yes' if the number is even, otherwise answer 'no'.",
+            "What is the result of the expression?",
+            "Find the greatest common divisor of given numbers.",
+            "What number is missing in the progression?",
+            "Answer 'yes' if given number is prime. Otherwise answer 'no'."
+        };
+    
+        if (numOfGames > 0 && numOfGames < rules.length && rules[numOfGames] != null) {
+            System.out.println(rules[numOfGames]);
+        } else if (numOfGames != 1) {
+            throw new IllegalStateException("Unexpected value: " + numOfGames);
         }
+    
         return userName;
     }
 
