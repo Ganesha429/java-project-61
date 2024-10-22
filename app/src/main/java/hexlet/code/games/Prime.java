5ec2e54e5c570cd1a49c8c49485ecc8b160ea027
Ganesha429 <132095053+Ganesha429@users.noreply.github.com>
2024-10-22 23:46:08 +0300

    Update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -7,37 +7,33 @@ import hexlet.code.Engine;
 public class Prime {
     public static void checkNumber(int numOfGame) {
         String newUserName = Engine.getName(numOfGame); // greeting and receiving the player's name
-
         int counterOfSuccessfulAttempts = 0;
         Random random = new Random();
 
         while (counterOfSuccessfulAttempts < 3) {
-            int randomNumber = random.nextInt(100); //will return a random number from 0 to 99
+            int randomNumber = random.nextInt(100); // will return a random number from 0 to 99
             System.out.println("Question: " + randomNumber);
 
             Scanner scanner = new Scanner(System.in);
             String answerOfUser = scanner.next();
             System.out.println("Your answer: " + answerOfUser);
 
-            int devideNumbersCounter = 0;
+            boolean isPrime = isPrime(randomNumber);
+            String correctAnswer = isPrime ? "yes" : "no";
 
-            // check: prime number or not
-            for (int number = 2; number < randomNumber / 2; ++number) {
-                if (randomNumber % number == 0) {
-                    devideNumbersCounter += 1;
-                }
-            }
-
-            //checking the player's answer
-            if (devideNumbersCounter == 0) {
-                counterOfSuccessfulAttempts = Engine.checkingForCorrectness(newUserName, answerOfUser, "yes",
-                        counterOfSuccessfulAttempts);
-            } else {
-                counterOfSuccessfulAttempts = Engine.checkingForCorrectness(newUserName, answerOfUser, "no",
-                        counterOfSuccessfulAttempts);
-            }
+            counterOfSuccessfulAttempts = Engine.checkingForCorrectness(newUserName, answerOfUser, correctAnswer, counterOfSuccessfulAttempts);
         }
+
         // congratulating the player when he wins
         Engine.checkingTheResultOfGame(newUserName, counterOfSuccessfulAttempts);
     }
+
+    private static boolean isPrime(int number) {
+        if (number < 2) return false;
+        for (int i = 2; i <= number / 2; ++i) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
 }
+
